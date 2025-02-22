a2807a95f5e8319a308e18020de7edbf8031035d
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-02-22 21:41:26 +0900

    caesars cipher

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # caesar-cipher
-ruby project for Odin
+Ruby project for Odin
+
+Dear reader,
+
+This is my first project in Ruby.
+
+It will not be as clean. But it works!
+
+Keep it simple.
+
+You can paste it to IRB.
---
+++ caesar_cipher.rb
@@ -0,0 +1,28 @@
+
+str = "Helloaa_yz"
+A_to_z = "abcdefghijklmnopqrstuvwxyz"
+Z_ord_val = "z".ord
+
+def cipher(string, shift_value)
+  string.chars.map do | char |
+    if A_to_z.include?(char.downcase)
+      char_value = char.downcase.ord + shift_value
+      pivoted_val = char_value > Z_ord_val ? char_value - 26 : char_value
+      char == char.downcase ? pivoted_val.chr : pivoted_val.chr.upcase
+    else
+      char
+    end
+  end.join
+end
+
+
+puts cipher(str, 2)
+puts cipher("What a string!", 5)
+puts cipher(A_to_z.upcase, 5)
+puts cipher(A_to_z, 5)
+
+
+
+
+
+
